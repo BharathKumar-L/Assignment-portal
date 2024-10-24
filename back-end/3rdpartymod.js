521d26156493fedb2ef64b3af1ea1ed0461054d9
Bharath Kumar L <142790392+BharathKumar-L@users.noreply.github.com>
2024-10-24 11:28:22 +0530

    Delete back-end directory

--- back-end/3rdpartymod.js
+++
@@ -1,8 +0,0 @@
-const fsy = require("fs");
-const details = fsy.readFile("data.txt", (err, data) => {
-  if (err) {
-    console.error("please enter valid name");
-  } else {
-    console.log(data.toString());
-  }
-});
--- back-end/module1.js
+++
@@ -1,4 +0,0 @@
-function module1(name)
-{
-    console.log(`Welcome to ${name}`);
-}module.exports =  module1;
