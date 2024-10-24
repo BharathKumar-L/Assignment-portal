402cebcf13306fed7ab0f40b775718ea00b12eef
Bharath Kumar L <142790392+BharathKumar-L@users.noreply.github.com>
2024-10-24 11:28:31 +0530

    Delete front-end directory

--- front-end/day-4/sum.js
+++
@@ -1,5 +0,0 @@
-function sum(){
-    var a= Number(document.getElementById("n1").value);
-    var b= Number(document.getElementById("n2").value);
-    document.getElementById("res").textContent = "Result " + (a+b);
-}
